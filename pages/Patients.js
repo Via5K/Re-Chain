15b5c01869e288304c760a70be988d0a5d7cbdc3
Via5K <1neerajnautiyal@gmail.com>
2022-06-23 10:45:06 +0530

    Merged Registertion Forms

--- pages/Patients.js
+++ pages/Patients.js
@@ -1,15 +1,22 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar.js';
 import PatientForm from '../components/forms/PatientForm.js';
+import PatientRegistrationForm from '../components/forms/PatientRegistrationForm.js';
 import Redirect from '../components/cards/Redirect.js';
 import useSession from '../components/hooks/useSession.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import PatientData from '../components/cards/PatientData.js';
+import styles from '../styles/cards.module.css';
 
 export default function Reports() {
   const router = useRouter();
   const { getItem } = useSession();
+
+  const [Form, setForm] = useState(true);
+
+  const getForm = () => setForm(true);
+  const addForm = () => setForm(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -22,8 +29,20 @@ export default function Reports() {
         <title>Patients </title>
       </Head>
       <Navbar />
-      {getItem('address') ? <PatientForm /> : <Redirect />}
-      {/* <PatientData /> */}
+      <div>
+        <div
+          className={`${styles.row}`}
+          style={{ maxWidth: '750px', margin: '0 auto' }}
+        >
+          <button onClick={getForm}>View Details</button>
+          <button onClick={addForm}>Register</button>
+        </div>
+      </div>
+      {getItem('address') ? (
+        <>{Form === true ? <PatientForm /> : <PatientRegistrationForm />}</>
+      ) : (
+        <Redirect />
+      )}
     </>
   );
 }
