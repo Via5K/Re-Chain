4350c90d7488a525432bf877a0adc831421e07bc
Via5K <72505269+Via5K@users.noreply.github.com>
2022-09-21 17:08:06 +0530

    V1.2 Upgrade - Wallet Address size 42

--- components/forms/PatientRegistrationForm.js
+++ components/forms/PatientRegistrationForm.js
@@ -91,7 +91,7 @@ export default function PatientRegistrationForm() {
     console.log(JSON.stringify(data));
 
     data.walletAddress = walletAddressRef.current.value;
-    if (data.walletAddress == '' || data.walletAddress.size != 40) {
+    if (data.walletAddress == '' || data.walletAddress.size == 42) {
       valid = false;
       msg = msg + "  Invalid Patient's Wallet Address,  ";
     }
